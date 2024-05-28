638dc661b7ca2841932d2bc25757f1a54b776f7a
Rocki <student@ML-RefVm-231318>
2024-05-28 11:50:55 -0400

    Add project files.

---
+++ JosephusProblemOOP/Program-1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace JosephusOO
+{
+    /// <summary>
+    /// Author: Rocki Taub
+    /// PA01
+    /// This program initializes a number of soldiers and a count number in order to implement Josephus's Problem in Object-Oriented code.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // default number of soldiers
+            int soldierNumber = 10;
+            if (args.Length == 1)
+            {
+                int.TryParse(args[0], out soldierNumber);
+            }
+
+            Soldier firstSoldier = CreateSoldiers(soldierNumber);
+         
+
+            firstSoldier.CommenceGroupSuicide(5);
+
+        }
+
+        /// <summary>
+        /// Creates the number of soldiers specified and sets Left and Right neighbors of soldiers.
+        /// </summary>
+        /// <param name="soldierNumber">number of soldiers to be instantiated</param>
+        /// <returns name="firstSoldier">first soldier in the circle</returns>
+        public static Soldier CreateSoldiers(int soldierNumber)
+        {
+            //prevSoldier stores previous soldier so can be set as the left of the next soldier
+            Soldier prevSoldier = null;
+            Soldier firstSoldier = null;
+
+
+            for (int ix = 0; ix < soldierNumber; ++ix)
+            {
+                String soldierName = "Private " + (ix + 1);
+
+                // generates ages of soldiers
+                var random = new Random();
+                int age = random.Next(20, 60);
+
+                Soldier soldier = new Soldier(soldierName, age, (ix + 1));
+
+                soldier.Left = prevSoldier;
+
+                // will set currentSoldier as right of previous soldier, or sets value of first soldier for later use.
+                if (ix != 0)
+                    prevSoldier.Right = soldier;
+                else
+                    firstSoldier = soldier;
+
+                // last soldier will have firstSoldier set as next Soldier to complete the circle
+                if (ix == (soldierNumber - 1))
+                {
+                    soldier.Right = firstSoldier;
+                    firstSoldier.Left = soldier;
+                }
+
+                prevSoldier = soldier;
+            }
+
+            return firstSoldier;
+
+        }
+    }
+}
+
+
---
+++ JosephusProblemOOP/Soldier-1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace JosephusOO
+{
+    /// <summary>
+    /// Soldier class initialized soldiers for Josephus's Problem. 
+    /// </summary>
+    class Soldier
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public int Location { get; set; } // numbered relative to the "first one created"
+        // neighbors:
+        public Soldier Left { get; set; }
+        public Soldier Right { get; set; }
+
+        /// <summary>
+        /// Soldier constructor initializes soldiers.
+        /// </summary>
+        /// <param name="na">name of soldier</param>
+        /// <param name="a">age of soldier</param>
+        /// <param name="loc">location of soldier relative to the first soldier</param>
+        public Soldier(String na, int a, int loc)
+        {
+            Name = na;
+            Age = a;
+            Location = loc;
+            Left = null;
+            Right = null;
+
+        }
+
+        /// <summary>
+        /// instructs currentSoldier to "kill" Right Neighbor by manipulating lefts and rights neighbors.
+        /// This will remove right neighbor from the circle.
+        /// </summary>
+        public void KillRightNeighbor()
+        {
+            Soldier executioner = this;
+            Soldier beingKilled = executioner.Right;
+            // executioner will now point to the next living soldier.
+            executioner.Right = beingKilled.Right;
+
+            // executioner's new neighbors will now point to executioner instead of beingKilled
+            executioner.Left.Right = executioner;
+            executioner.Right.Left = executioner;
+
+            Console.WriteLine($"Executing {beingKilled.Name}, age {beingKilled.Age}.");
+        }
+        /// <summary>
+        /// Loops through all the soldiers and kills every time countdown becomes 0.
+        /// When only one soldier is left, the loop exits and the survivor is printed.
+        /// </summary>
+        /// <param name="n">soldiers countdown from n till 0 which is when  KillRightNeighbor() will be called.</param>
+        public void CommenceGroupSuicide(int n) 
+        {
+            // starts with n - 1, in order to start the countdown from first soldier, and not the next one.
+            int countdown = n - 1;
+            Soldier currSoldier = this;
+
+            // while the currentSoldier is not the only one in the circle
+            while (currSoldier.Right != currSoldier)
+            {
+                if (countdown == 0)
+                {
+                    countdown = n;
+                    currSoldier.KillRightNeighbor();
+                }                    
+                countdown--;          
+                currSoldier = currSoldier.Right;                           
+            }
+            Console.WriteLine($"Survivor: {currSoldier.Name}, age {currSoldier.Age}. ");
+        }
+
+        /// <summary>
+        /// Prints name, age, and location of soldier
+        /// </summary>
+        /// <returns>string information of soldier</returns>
+        public string toString()
+        {
+            return ($"Name: {Name} Age: {Age} Location: {Location}");
+        }
+
+    }
+}
